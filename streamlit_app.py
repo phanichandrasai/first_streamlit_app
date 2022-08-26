178f44e1e37b6476b7a2543c02b72eb77570e9bc
Phanichandrasai Vellanki <112181612+phanichandrasai@users.noreply.github.com>
2022-08-26 10:14:56 -0400

    Update streamlit_app.py
    Change the Breakfast header name and added new line with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healty Dinner")
-streamlit.header("Breakfast Menu")
+streamlit.header("Breakfast Favorites")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale,Spinach & Rocket Smoothie")
 streamlit.text("Hard-Boliled Free-Range Egg")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
