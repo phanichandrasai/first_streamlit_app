ad2a023bdac9d1bedd4a7f009fb012dd4f0f50b6
Phanichandrasai Vellanki <112181612+phanichandrasai@users.noreply.github.com>
2022-08-27 17:14:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,6 @@ streamlit.dataframe(my_data_row)
 
 
 second_choice = streamlit.text_input('What fruit would you like add?','')
-streamlit.write('The user entered ', second_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ second_choice)
 streamlit.text("Thanks for adding the" + second_choice")
 
