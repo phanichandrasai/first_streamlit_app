378d615e9d1bf11e6e9a80632c49e51c611b24c5
Phanichandrasai Vellanki <112181612+phanichandrasai@users.noreply.github.com>
2022-08-27 22:17:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ except URLError as e:
 
 if streamlit.button ('Get Fruit List:'):
               my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]
-              my_data_row = get_fruit_list()
+              my_data_rows = get_fruit_list()
               my_cnx.close()
               streamlit.dataframe(my_data_row)
 
